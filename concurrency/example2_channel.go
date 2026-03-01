2dac8218a09099bbf243c4acaf67044b970f649f
ayuxsec <ayuxsec@proton.me>
2026-03-01 09:34:52 +0530

    Create example2_channel.go

---
+++ concurrency/example2_channel.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num1 := 5
+	num2 := 6
+
+	c := make(chan int)
+
+	go add(num1, num2, c)
+
+	result := <-c  // recieve from c (block until c is recieved)
+	fmt.Println(result) // 11
+}
+
+func add(x, y int, c chan int) {
+	c <- x + y // send to channel
+}
